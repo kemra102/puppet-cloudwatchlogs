cfa80f212a4ca296b037c5d66c903489e69aaf98
Danny Roberts <danny@thefallenphoenix.net>
2015-04-17 20:41:45 +0100

    Added support for none Amazon support Linux operating systems (Ubuntu, CentOS & RedHat according to: http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/QuickStartEC2Instance.html).

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@
 # Here you should define a list of variables that this module would require.
 #
 # [*logs*]
-#   A hash of arrays containg the 'name' & the 'path' of the log file(s) of the 
+#   A hash of arrays containg the 'name' & the 'path' of the log file(s) of the
 #   log file(s) to be sent to Cloudwatch Logs.
 #
 # [*region*]
@@ -38,23 +38,16 @@
 #
 class cloudwatchlogs {
 
-  # Check the OS is Amazon Linux
-  case $::operatingsystem {
-    'Amazon': {
-      $logs = [
-        { name => 'Messages', path => '/var/log/messages', },
-        { name => 'Secure', path => '/var/log/secure', },
-      ]
-    }
-    default: { fail("The ${module_name} module is not supported on 
-      ${::osfamily}/${::operatingsystem}.") }
-  }
-  
   # Set some default variables
+  $logs = [
+    { name => 'Messages', path => '/var/log/messages', },
+    { name => 'Secure', path => '/var/log/secure', },
+  ]
+
   $region                = undef
   $aws_access_key_id     = undef
   $aws_secret_access_key = undef
-  
+
   # Validate variables using puppet/stdlib
   validate_hash($logs)
   validate_string($region)
@@ -66,16 +59,8 @@ class cloudwatchlogs {
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    require => Package['awslogs'],
-    before  => Service['awslogs'],
-    notify  => Service['awslogs'],
   }
-  
-  # Install the cloudwatchlogs package
-  package { 'awslogs':
-    ensure => 'installed',
-  }
-  
+
   # Manage the cloudwatchlogs config files
   file { '/etc/awslogs/awslogs.conf':
     ensure  => 'file',
@@ -85,7 +70,7 @@ class cloudwatchlogs {
     ensure  => 'file',
     content => template('cloudwatchlogs/awscli.conf.erb'),
   }
-  
+
   # Start the clouwatchlogs service
   service { 'awslogs':
     ensure     => 'running',
@@ -93,5 +78,53 @@ class cloudwatchlogs {
     hasrestart => true,
     hasstatus  => true,
   }
-  
+
+  case $::operatingsystem {
+    'Amazon': {
+      # Set some File type defaults
+      File {
+        require => Package['awslogs'],
+        notify  => Service['awslogs'],
+      }
+      # Install the cloudwatchlogs package
+      package { 'awslogs':
+        ensure => 'installed',
+      }
+    }
+    /^(Ubuntu|CentOS|RedHat)$/: {
+      # We need wget to fetch the installation script
+      package { 'wget':
+        ensure => 'present',
+      }
+      # Grab the installation script
+      exec { 'cloudwatchlogs-wget':
+        path    => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
+        command => 'wget -O /usr/local/src/awslogs-agent-setup.py https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py',
+        unless  => '[ -e /usr/local/src/awslogs-agent-setup.py ]',
+      }
+
+      # Create the /etc/awslogs directory manually
+      file { '/etc/awslogs':
+        ensure => 'directory',
+        mode   => '0755',
+        before => [
+          File['/etc/awslogs/awslogs.conf'],
+          File['/etc/awslogs/awscli.conf'],
+        ],
+      }
+      # Install cloudwatchlogs
+      exec { 'cloudwatchlogs-install':
+        path    => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
+        command => "python /usr/local/src/awslogs-agent-setup.py -n -r ${region} -c /etc/awslogs/awslogs.conf",
+        onlyif  => '[ -e /usr/local/src/awslogs-agent-setup.py ]',
+        unless  => '[ -f /var/awslogs/etc/awslogs.conf ]',
+        require => [
+          File['/etc/awslogs/awslogs.conf'],
+          File['/etc/awslogs/awscli.conf'],
+        ],
+        before  => Service['awslogs'],
+      }
+    }
+  }
+
 }
