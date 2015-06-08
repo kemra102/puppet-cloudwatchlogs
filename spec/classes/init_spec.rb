cb08642ed94e8294380a7932f37a25bf2bec6d12
Danny Roberts <danny.roberts@reconnix.com>
2015-06-08 17:29:57 +0100

    First pass at new test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,36 +7,20 @@ describe 'cloudwatchlogs', :type => :class do
     it {
       should create_class('cloudwatchlogs')
       should contain_package('awslogs').with_ensure('present')
-      should contain_file('/etc/awslogs/awslogs.conf').with({
-        'ensure' => 'file',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0644',
+      should contain_concat('/etc/awslogs/awslogs.conf').with({
+        'ensure'         => 'file',
+        'owner'          => 'root',
+        'group'          => 'root',
+        'mode'           => '0644',
+        'ensure_newline' => 'true',
+        'warn'           => 'true',
       })
-      should contain_file('/etc/awslogs/awslogs.conf').with_content(/state_file = \/var\/lib\/awslogs\/agent-stat/)
+      should contain_concat('/etc/awslogs/awslogs.conf').with_content(/state_file = \/var\/lib\/awslogs\/agent-stat/)
       should contain_service('awslogs').with({
         'ensure'     => 'running',
         'enable'     => 'true',
         'hasrestart' => 'true',
         'hasstatus'  => 'true',
-      })
-    }
-  end
-  context 'set aws region & keys' do
-    let (:params) {{
-      :region                => 'eu-west-1',
-      :aws_access_key_id     => 'AKIAIOSFODNN7EXAMPLE',
-      :aws_secret_access_key => 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
-    }}
-    let (:facts) {{
-      :operatingsystem => 'Amazon',
-    }}
-    it {
-      should contain_file('/etc/awslogs/awscli.conf').with({
-        'ensure' => 'file',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0644',
       })
     }
   end
@@ -60,36 +44,32 @@ describe 'cloudwatchlogs', :type => :class do
         'group'  => 'root',
         'mode'   => '0755',
       })
-      should contain_file('/var/awslogs').with({
-        'ensure' => 'directory',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0755',
+      should contain_concat('/etc/awslogs/awslogs.conf').with({
+        'ensure'         => 'file',
+        'owner'          => 'root',
+        'group'          => 'root',
+        'mode'           => '0644',
+        'ensure_newline' => 'true',
+        'warn'           => 'true',
       })
-      should contain_file('/var/awslogs/etc').with({
-        'ensure' => 'directory',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0755',
-      })
-      should contain_file('/etc/awslogs/awslogs.conf').with({
-        'ensure' => 'file',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0644',
-      })
-      should contain_file('/etc/awslogs/awslogs.conf').with_content(/state_file = \/var\/awslogs\/state\/agent-stat/)
+      should contain_concat('/etc/awslogs/awslogs.conf').with_content(/state_file = \/var\/awslogs\/state\/agent-stat/)
+      should contain_file('/var/awslogs').with_ensure('directory')
+      should contain_file('/var/awslogs/etc').with_ensure('directory')
       should contain_file('/var/awslogs/etc/awslogs.conf').with({
-        'ensure' => 'file',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0644',
+        'ensure' => 'link',
+        'target' => '/etc/awslogs/awslogs.conf',
       })
       should contain_exec('cloudwatchlogs-install').with({
         'path'    => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
         'command' => 'python /usr/local/src/awslogs-agent-setup.py -n -r eu-west-1 -c /etc/awslogs/awslogs.conf',
         'onlyif'  => '[ -e /usr/local/src/awslogs-agent-setup.py ]',
         'unless'  => '[ -d /var/awslogs/bin ]',
+      })
+      should contain_service('awslogs').with({
+        'ensure'     => 'running',
+        'enable'     => 'true',
+        'hasrestart' => 'true',
+        'hasstatus'  => 'true',
       })
     }
   end
