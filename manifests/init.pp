dac0c16c3ad4e5fb732e4eaa48db1911bf810834
AdamCousins <Adam@KAPSYD-LTADAM.au.kaplan.local>
2016-05-09 15:50:45 +0800

    fix errors test build discovered

--- manifests/init.pp
+++ manifests/init.pp
@@ -163,11 +163,11 @@ class cloudwatchlogs (
   }
 
   if $log_level {
-    file { "${logging_config_file}":
-        ensure         => 'present',
-        owner          => 'root',
-        group          => 'root',
-        mode           => '0644',
+    file { '/etc/awslogs/awslogs_dot_log.conf' :
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
         content => template('cloudwatchlogs/awslogs_logging_config_file.erb'),
     }
   }
