be8d18a29828a5446e822075cce3da8fde3cd44d
tbugfinder <github@online.ms>
2018-04-12 15:42:58 +0200

    AmazonLinux2 LTS is using different service name

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,14 @@ class cloudwatchlogs::params {
     'Amazon': { $state_file = '/var/lib/awslogs/agent-state' }
     default: { $state_file = '/var/awslogs/state/agent-state' }
   }
+  $osname = $facts['os']['name']
+  $osmajor = $facts['os']['release']['major']
+  $oslong = "${osname}${osmajor}"
+  
+  case $oslong {
+    'Amazon2': { $service_name = 'awslogsd' }
+    default: { $service_name = 'awslogs' }
+  }
   $logging_config_file = '/etc/awslogs/awslogs_dot_log.conf'
   $region = undef
   $log_level = undef
